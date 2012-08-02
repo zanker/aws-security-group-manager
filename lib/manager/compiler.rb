715db2afab8c9363f661b645509abfdeb7e540e2
Zachary Anker <zach.anker@gmail.com>
2012-08-01 17:47:34 -0700

    Implemented all of the code for pulling and compiling the security group rules for RDS and EC2

--- lib/manager/compiler.rb
+++ lib/manager/compiler.rb
@@ -1,26 +1,74 @@
 module AWSSecurityGroups
   class Compiler
-    def initialize(groups, servers)
-      @groups, @servers = groups, servers
+    def initialize(security_groups, servers)
+      @security_groups, @servers = security_groups, servers
     end
 
-    def build(product, group_name, config)
-      if product == :ec2
-        build_ec2(group_name, config)
-      elsif product == :rds
-        build_rds(group_name, config)
-      else
+    def build(product, region, group_name, group_config)
+      unless product == "ec2" or product == "rds"
         raise "Unknown or unsupported AWS product #{product}"
       end
+
+      compiled = []
+
+      group_config.each do |config|
+        if product == "ec2"
+          next unless config["protocol"] and config["port"]
+        end
+
+        # We're configuring a group to have access
+        if config["group"]
+          groups, ips = self.find_servers(region, config)
+          groups.each do |group|
+            compiled.push(:group => group, :port => config["port"], :protocol => config["protocol"])
+          end
+
+        # We're configuring an IP range to have access
+        elsif config["ip"]
+          ips = [config["ip"]]
+        end
+
+        ips.each do |ip|
+          compiled.push(:ip => ip, :port => config["port"], :protocol => config["protocol"])
+        end
+      end
+
+      compiled
     end
 
-    private
-    def build_ec2(group_name, config)
+    # Find servers and groups matching the ruleset given
+    def find_servers(region, config)
+      groups, ips = [], []
 
-    end
+      @servers.each do |server_region, list|
+        # Only add servers that are part of the security group in the same region as this security group
+        if config["region"] == :SAME and region != server_region
+          next
+        end
 
-    def build_rds(group_name, config)
-      
+        # Only add servers that are part of the security group in a specific region
+        if config["region"].is_a?(String) and config["region"] != server_region
+          next
+        end
+
+        list.each do |instance_id, instance|
+          # We're filtering by a specific group
+          if config["group"].is_a?(String) and !instance[:group_names].include?(config["group"])
+            next
+          end
+
+          # Same region, so add the security group to itself
+          if server_region == region
+            groups.push(config["group"])
+          # Different region, authorize the IP
+          else
+            ips.push("#{instance[:ip_address]}/32")
+          end
+        end
+      end
+
+      groups.uniq!
+      return groups, ips
     end
   end
 end
--- lib/manager/core.rb
+++ lib/manager/core.rb
@@ -2,6 +2,8 @@ require "fog"
 require "yaml"
 
 module AWSSecurityGroups
+  attr_reader :servers
+
   class Core
     def initialize(access_key, secret_key)
       @access_key, @secret_key = access_key, secret_key
@@ -13,44 +15,81 @@ module AWSSecurityGroups
     end
 
     def load_security_groups
-      @security_groups = {}
+      @security_groups = {"ec2" => {}, "rds" => {}}
+
+      # EC2
       @regions.each do |region|
-        @security_groups[region] = {}
+        @security_groups["ec2"][region] = {}
 
         aws = Fog::Compute.new(:provider => "AWS", :region => region, :aws_access_key_id => @access_key, :aws_secret_access_key => @secret_key)
         aws.security_groups.each do |group|
-          @security_groups[region][group.group_id] = {:name => group.name, :owner_id => group.owner_id, :permissions => group.ip_permissions}
+          @security_groups["ec2"][region][group.name] = {:group_id => group.group_id, :owner_id => group.owner_id, :permissions => group.ip_permissions}
         end
 
-        @security_groups.delete(region) if @security_groups[region].empty?
+        @security_groups["ec2"].delete(region) if @security_groups["ec2"][region].empty?
+      end
+
+      # RDS
+      @regions.each do |region|
+        @security_groups["rds"][region] = {}
+
+        rds = Fog::AWS::RDS.new(:region => region, :aws_access_key_id => @access_key, :aws_secret_access_key => @secret_key)
+        rds.describe_db_security_groups.body["DescribeDBSecurityGroupsResult"]["DBSecurityGroups"].each do |group|
+          @security_groups["rds"][region][group["DBSecurityGroupName"]] = {:owner_id => group["OwnerId"], :ec2 => group["EC2SecurityGroups"], :ip => group["IPRanges"]}
+        end
       end
     end
 
     def load_servers
-      @servers = {}
-      @security_groups.each_key do |region|
-        @servers[region] = {}
+      @servers = {"ec2" => {}, "rds" => {}}
+
+      # Load a list of EC2 servers
+      @regions.each do |region|
+        @servers["ec2"][region] = {}
 
         aws = Fog::Compute.new(:provider => "AWS", :region => region, :aws_access_key_id => @access_key, :aws_secret_access_key => @secret_key)
         aws.describe_instances("instance-state-name" => "running").body["reservationSet"].each do |instance|
           instance_set = instance["instancesSet"].first
-          @servers[region][instance_set["instanceId"]] = {:group_names => instance_set["groupSet"], :group_ids => instance_set["groupIds"], :ip_address => instance_set["ipAddress"]}
+          @servers["ec2"][region][instance_set["instanceId"]] = {:group_names => instance["groupSet"], :group_ids => instance["groupIds"], :ip_address => instance_set["ipAddress"]}
         end
 
-        # Assuming if you don't have any servers in the region, you don't care about it
-        if @servers[region].empty?
-          @servers.delete(region)
-          @security_groups.delete(region)
+        @servers["ec2"].delete(region) if @servers["ec2"][region].empty?
+      end
+
+      # Now RDS
+      @regions.each do |region|
+        @servers["rds"][region] = {}
+
+        rds = Fog::AWS::RDS.new(:region => region, :aws_access_key_id => @access_key, :aws_secret_access_key => @secret_key)
+        rds.describe_db_instances.body["DescribeDBInstancesResult"]["DBInstances"].each do |instance|
+          group_names = []
+          instance["DBSecurityGroups"].each do |group|
+            group_names.push(group["DBSecurityGroupName"]) if group["Status"] == "active"
+          end
+
+          @servers["rds"][region][instance["DBInstanceIdentifier"]] = {:group_names => group_names}
         end
+
+        @servers["rds"].delete(region) if @servers["rds"][region].empty?
       end
     end
 
-    def build_security_groups(product, config)
-      compiler = Compiler.new(@security_groups, @servers)
+    def compile_security_groups(product, settings)
+      compiler = Compiler.new(@security_groups[product], @servers["ec2"])
 
       new_groups = {}
-      config.each do |group_name, group|
-        new_groups[group_name] = compiler.build(product, group_name, group)
+      settings.each do |group_name, group_config|
+        new_groups[group_name] = {}
+
+        @regions.each do |region|
+          # Don't set security groups if the region has no servers
+          # Or if the region doesn't actually have the security group, that would be pointless
+          unless @servers[product][region] and @security_groups[product][region] and @security_groups[product][region][group_name]
+            next
+          end
+
+          new_groups[group_name][region] = compiler.build(product, region, group_name, group_config)
+        end
       end
 
       new_groups
